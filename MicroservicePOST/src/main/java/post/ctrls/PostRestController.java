64d1c631a1672f90ea87705cc4e1922aa74c4d69
vukmanovicmilos <vukmanovic.milos@gmail.com>
2019-03-21 11:33:35 +0100

    Dopuna

--- MicroservicePOST/src/main/java/post/ctrls/PostRestController.java
+++ MicroservicePOST/src/main/java/post/ctrls/PostRestController.java
@@ -1,5 +1,65 @@
 package post.ctrls;
 
-public class PostRestController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import post.jpa.Artikl;
+import post.jpa.Dobavljac;
+import post.jpa.Porudzbina;
+import post.jpa.StavkaPorudzbine;
+import post.reps.ArtiklRepository;
+import post.reps.DobavljacRepository;
+import post.reps.PorudzbinaRepository;
+import post.reps.StavkaPorudzbineRepository;
+
+@RestController
+public class PostRestController {
+	@Autowired
+	private ArtiklRepository artiklRepository;
+
+	@Autowired
+	private DobavljacRepository dobavljacRepository;
+
+	@Autowired
+	private PorudzbinaRepository porudzbinaRepository;
+
+	@Autowired
+	private StavkaPorudzbineRepository stavkaPorudzbineRepository;
+
+	@PostMapping("artikl")
+	public ResponseEntity<Artikl> insertArtikl(@RequestBody Artikl artikl) {
+		if (artiklRepository.existsById(artikl.getId()))
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		artiklRepository.save(artikl);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PostMapping("dobavljac")
+	public ResponseEntity<Dobavljac> insertDobavljac(@RequestBody Dobavljac dobavljac) {
+		if (dobavljacRepository.existsById(dobavljac.getId()))
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		dobavljacRepository.save(dobavljac);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PostMapping("porudzbina")
+	public ResponseEntity<Porudzbina> insertPorudzbina(@RequestBody Porudzbina porudzbina) {
+		if (porudzbinaRepository.existsById(porudzbina.getId()))
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		porudzbinaRepository.save(porudzbina);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PostMapping("stavkaPorudzbine")
+	public ResponseEntity<StavkaPorudzbine> insertStavkaPorudzbine(@RequestBody StavkaPorudzbine stavkaPorudzbine) {
+		if (stavkaPorudzbineRepository.existsById(stavkaPorudzbine.getId()))
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		stavkaPorudzbine.setRedniBroj(stavkaPorudzbineRepository.nextRBr(stavkaPorudzbine.getPorudzbinaBean().getId()));
+		stavkaPorudzbineRepository.save(stavkaPorudzbine);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 }
--- MicroservicePOST/src/main/java/post/reps/StavkaPorudzbineRepository.java
+++ MicroservicePOST/src/main/java/post/reps/StavkaPorudzbineRepository.java
@@ -1,9 +1,12 @@
 package post.reps;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import post.jpa.StavkaPorudzbine;
 
 public interface StavkaPorudzbineRepository extends JpaRepository<StavkaPorudzbine, Integer> {
+	@Query(value = "select coalesce(max(redni_broj)+1, 1) from stavka_porudzbine where porudzbina = ?1", nativeQuery = true)
+	Integer nextRBr(int porudzbinaId);
 
 }
