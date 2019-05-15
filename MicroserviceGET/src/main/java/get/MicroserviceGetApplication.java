51b8aa3b3f2c85a992ba3acf7aa9bb31eb76a607
vukmanovicmilos <vukmanovic.milos@gmail.com>
2019-05-15 13:53:43 +0200

    Zuul, Spring security

--- MicroserviceGET/src/main/java/get/MicroserviceGetApplication.java
+++ MicroserviceGET/src/main/java/get/MicroserviceGetApplication.java
@@ -3,6 +3,10 @@ package get;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceGetApplication {
--- ZuulAPIGateway/src/main/java/zuul/ZuulApiGatewayApplication.java
+++ ZuulAPIGateway/src/main/java/zuul/ZuulApiGatewayApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
